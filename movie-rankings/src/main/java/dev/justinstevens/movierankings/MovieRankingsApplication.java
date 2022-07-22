15fa1f8b8962e3fd2d6e21b4dbe5348f7a30147b
Justin Stevens <49693694+JSteve0@users.noreply.github.com>
2022-07-21 23:38:20 -0400

    Add files via upload

---
+++ movie-rankings/src/main/java/dev/justinstevens/movierankings/MovieRankingsApplication.java
@@ -0,0 +1,13 @@
+package dev.justinstevens.movierankings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRankingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRankingsApplication.class, args);
+	}
+
+}
